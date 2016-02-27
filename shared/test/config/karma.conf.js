8e10d88b3c85729669eb23d1bb45553dad3f69cd
Andreas Eichner <andreas.eichner@quartett-mobile.de>
2016-02-27 12:18:04 +0100

    Modified file structure for tests

--- shared/test/config/karma.conf.js
+++ shared/test/config/karma.conf.js
@@ -25,17 +25,16 @@ module.exports = function(config) {
         'node_modules/requirejs/require.js',
     	'app/js/formulargenerator.js',
 //      'app/js/backendconnector.js',
-
-//tests
-      'shared/test/test.js',
-      'shared/test/testangtospectransform.js',
-      
-      'shared/js/jsontransformer.js',
-      'shared/js/transformMethods.js',
+        'shared/js/jsontransformer.js',
+        'shared/js/transformMethods.js',
+        
+        //tests
+        'shared/test/testspectoangtransform.js',
+        'shared/test/testangtospectransform.js',
 
         // fixtures
-        {pattern: 'shared/test/testcases/*.json', watched: true, served: true, included: false},
-      {pattern: 'shared/test/testcases/af_to_idp_spec/*.json', watched: true, served: true, included: false}
+        {pattern: 'shared/test/testcases/idp_spec_to_af/*.json', watched: true, served: true, included: false},
+        {pattern: 'shared/test/testcases/af_to_idp_spec/*.json', watched: true, served: true, included: false}
     ],
 
 
